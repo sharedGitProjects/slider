f14e7899e0634b504dbe4cae295788d533d7a225
sharedGitProjects <43945686+sharedGitProjects@users.noreply.github.com>
2018-10-12 07:42:53 +0400

    Update script.js

--- script.js
+++ script.js
@@ -18,258 +18,260 @@ class LinkBuilder {
     }
 }
 
-function SliderJS(containerId, options = {}) {
-    this._topContainer = null;
-    this._slides = [];
-    this._currentSlideIndex = 0;
-    this._options = options;
-    this._containerId = containerId;
-    this._autoplayKey = null;
-    this._width = 800;
-    this._height = 400;
-    this._playing = false;
-    this._linkBuilder = new LinkBuilder();
-    this.init();
-};
-
-SliderJS.prototype.init = function () {
-    if (!this._containerId) {
-        console.error('Не задан ИД контейнера картинок');
-        return;
-    }
-
-    this._topContainer = document.getElementById(this._containerId);
-    if (!this._topContainer) {
-        console.error('Не найден контейнер картинок');
-        return;
-    }
-
-    this.initSlides();
-    this.initContainer();
-    if (!this._options.hideControls) {
-        this.addPrevLink();
-        this.addNextLink();
-
-        if (this._options.autoplay) {
-            this.addAutoplay();
-            this.addPause();
-        }
-    } else if (this._options.autoplay) {
-        this.autoplay();
-    }
-};
-
-SliderJS.prototype.initContainer = function () {
-    var self = this;
-    let slidesContainer = document.createElement("div");
-    slidesContainer.classList.add("slidesjs-container");
-    slidesContainer.style.width = `${this._width}px`;
-    slidesContainer.style.height = `${this._height}px`;
-    slidesContainer.addEventListener("transitionend", function() {
-        self._playing = false;
-    });
-
-    let slidesControl = document.createElement("div");
-    slidesControl.classList.add("slidesjs-control");
-    slidesControl.style.width = `${this._width}px`;
-    slidesControl.style.height = `${this._height}px`;
-    slidesContainer.appendChild(slidesControl);
-
-    for (let i = 0; i < this._slides.length; i++) {
-        slidesControl.appendChild(this._slides[i]);
-    }
-
-    this._topContainer.appendChild(slidesContainer);
-};
-
-SliderJS.prototype.initSlides = function () {
-    this._topContainer.classList.add("slides");
+class SliderJS {
+    constructor(containerId, options = {}) {
+        this._topContainer = null;
+        this._slides = [];
+        this._currentSlideIndex = 0;
+        this._options = options;
+        this._containerId = containerId;
+        this._autoplayKey = null;
+        this._width = 800;
+        this._height = 400;
+        this._playing = false;
+        this._linkBuilder = new LinkBuilder();
+        this.init();
+    };
     
-    let slides = this._topContainer.querySelectorAll('.slides > div,img');
-    if (slides.length === 0) {
-        return;
-    }
-
-    let prevIndex = slides.length - 1;
-    for (let i = 0; i < slides.length; i++) {
-        let slide = slides[i];
-        slide.classList.add("slidesjs-slide");
-
-        if (i > 1 && i != prevIndex) {
-            slide.style.display = 'none';
+    init() {
+        if (!this._containerId) {
+            console.error('Не задан ИД контейнера картинок');
+            return;
         }
 
-        if (i === prevIndex) {
-            slide.style.left = `-${this._width}px`;
+        this._topContainer = document.getElementById(this._containerId);
+        if (!this._topContainer) {
+            console.error('Не найден контейнер картинок');
+            return;
         }
 
-        if (i === 1) {
-            slide.style.left = `${this._width}px`;
+        this.initSlides();
+        this.initContainer();
+        if (!this._options.hideControls) {
+            this.addPrevLink();
+            this.addNextLink();
+
+            if (this._options.autoplay) {
+                this.addAutoplay();
+                this.addPause();
+            }
+        } else if (this._options.autoplay) {
+            this.autoplay();
+        }
+    };
+
+    initContainer() {
+        var self = this;
+        let slidesContainer = document.createElement("div");
+        slidesContainer.classList.add("slidesjs-container");
+        slidesContainer.style.width = `${this._width}px`;
+        slidesContainer.style.height = `${this._height}px`;
+        slidesContainer.addEventListener("transitionend", function() {
+            self._playing = false;
+        });
+
+        let slidesControl = document.createElement("div");
+        slidesControl.classList.add("slidesjs-control");
+        slidesControl.style.width = `${this._width}px`;
+        slidesControl.style.height = `${this._height}px`;
+        slidesContainer.appendChild(slidesControl);
+
+        for (let i = 0; i < this._slides.length; i++) {
+            slidesControl.appendChild(this._slides[i]);
         }
 
-        if (i === 0) {
-            slide.style.left = "0px"
-        }
+        this._topContainer.appendChild(slidesContainer);
+    };
 
-        this._slides.push(slide);
-    }
-};
-
-SliderJS.prototype.addLink = function (className, innerHTML, action) {
-    let link = this._linkBuilder.buildLink(className, innerHTML, action);
-    this._topContainer.appendChild(link);
-    return link;
-};
-
-SliderJS.prototype.addAutoplay = function () {
-    this.addLink('slidesjs-autoplay', '►', this.autoplay.bind(this));
-};
-
-SliderJS.prototype.addPause = function () {
-    let link = this.addLink('slidesjs-pause', '❚❚', this.pause.bind(this));
-    link.style.display = 'none';
-};
-
-SliderJS.prototype.addPrevLink = function () {
-    this.addLink('slidesjs-previous', '←', this.prev.bind(this));
-};
-
-SliderJS.prototype.addNextLink = function () {
-    this.addLink('slidesjs-next', '→', this.next.bind(this));
-};
-
-SliderJS.prototype.getPrevIndex = function (currentIndex) {
-    if (this._slides.length === 0) {
-        return 0;
-    }
+    initSlides() {
+        this._topContainer.classList.add("slides");
     
-    let prevIndex;
-    if (currentIndex === 0) {
-        prevIndex = this._slides.length - 1;
-    } else {
-        prevIndex = currentIndex - 1;
-    }
-    return prevIndex;
-};
+        let slides = this._topContainer.querySelectorAll('.slides > div,img');
+        if (slides.length === 0) {
+            return;
+        }
 
-SliderJS.prototype.getNextIndex = function (currentIndex) {
-    if (this._slides.length === 0) {
-        return 0;
-    }
+        let prevIndex = slides.length - 1;
+        for (let i = 0; i < slides.length; i++) {
+            let slide = slides[i];
+            slide.classList.add("slidesjs-slide");
 
-    let nextIndex;
-    if (currentIndex < this._slides.length - 1) {
-        nextIndex = currentIndex + 1;
-    } else {
-        nextIndex = 0;
-    }
-    return nextIndex;
-};
+            if (i > 1 && i != prevIndex) {
+                slide.style.display = 'none';
+            }
 
-SliderJS.prototype.autoplay = function () {
-    if (this._slides.length < 1) {
-        return;
-    }
+            if (i === prevIndex) {
+                slide.style.left = `-${this._width}px`;
+            }
 
-    let autoplayLink = this._topContainer.querySelector('.slidesjs-autoplay');
-    if (autoplayLink) {
-        autoplayLink.style.display = 'none'
-    }
+            if (i === 1) {
+                slide.style.left = `${this._width}px`;
+            }
 
-    let pauseLink = this._topContainer.querySelector('.slidesjs-pause');
-    if (pauseLink) {
-        pauseLink.style.display = 'inline'
-    }
+            if (i === 0) {
+                slide.style.left = "0px"
+            }
 
-    let interval = this._options.autoplayInterval;
-    if (!interval || interval < 1000) {
-        interval = 1000;
-    }
+            this._slides.push(slide);
+        }
+    };
 
-    this.next();
-    this._autoplayKey = setInterval(this.next.bind(this), interval);
-};
+    addLink(className, innerHTML, action) {
+        let link = this._linkBuilder.buildLink(className, innerHTML, action);
+        this._topContainer.appendChild(link);
+        return link;
+    };
 
-SliderJS.prototype.pause = function () {
-    if (!this._autoplayKey) {
-        return;
-    }
+    addAutoplay() {
+        this.addLink('slidesjs-autoplay', '►', this.autoplay.bind(this));
+    };
 
-    clearInterval(this._autoplayKey);
-    this._autoplayKey = null;
+    addPause() {
+        let link = this.addLink('slidesjs-pause', '❚❚', this.pause.bind(this));
+        link.style.display = 'none';
+    };
 
-    let autoplayLink = this._topContainer.querySelector('.slidesjs-autoplay');
-    if (autoplayLink) {
-        autoplayLink.style.display = 'inline'
-    }
+    addPrevLink() {
+        this.addLink('slidesjs-previous', '←', this.prev.bind(this));
+    };
 
-    let pauseLink = this._topContainer.querySelector('.slidesjs-pause');
-    if (pauseLink) {
-        pauseLink.style.display = 'none'
-    }
-};
+    addNextLink() {
+        this.addLink('slidesjs-next', '→', this.next.bind(this));
+    };
 
-SliderJS.prototype.prev = function () {
-    if (this._slides.length < 1) {
-        return;
-    }
-
-    if (this._playing) 
-    {
-        return;
-    }
-
-    this._playing = true;
+    getPrevIndex(currentIndex) {
+        if (this._slides.length === 0) {
+            return 0;
+        }
     
-    let nextIndex = this.getNextIndex(this._currentSlideIndex);
-    this._slides[nextIndex].style.display = 'none';
+        let prevIndex;
+        if (currentIndex === 0) {
+            prevIndex = this._slides.length - 1;
+        } else {
+            prevIndex = currentIndex - 1;
+        }
+        return prevIndex;
+    };
 
-    this._slides[this._currentSlideIndex].style.left = `${this._width}px`;
-    this._slides[this._currentSlideIndex].style.zIndex = '10';
+    getNextIndex(currentIndex) {
+        if (this._slides.length === 0) {
+            return 0;
+        }
 
-    let prevIndex = this.getPrevIndex(this._currentSlideIndex);
+        let nextIndex;
+        if (currentIndex < this._slides.length - 1) {
+            nextIndex = currentIndex + 1;
+        } else {
+            nextIndex = 0;
+        }
+        return nextIndex;
+    };
 
-    let prevPrevIndex = this.getPrevIndex(prevIndex);
+    autoplay() {
+        if (this._slides.length < 1) {
+            return;
+        }
 
-    let nextNextIndex = this.getNextIndex(nextIndex);
-    if (this._slides.length > 3) {
-        this._slides[prevPrevIndex].style.display = 'block';
-    }
-    this._slides[prevPrevIndex].style.left = `-${this._width}px`;
+        let autoplayLink = this._topContainer.querySelector('.slidesjs-autoplay');
+        if (autoplayLink) {
+            autoplayLink.style.display = 'none'
+        }
 
-    this._slides[prevIndex].style.display = 'block';
-    this._slides[prevIndex].style.left = '0px';
+        let pauseLink = this._topContainer.querySelector('.slidesjs-pause');
+        if (pauseLink) {
+            pauseLink.style.display = 'inline'
+        }
 
-    this._currentSlideIndex = prevIndex;
-};
+        let interval = this._options.autoplayInterval;
+        if (!interval || interval < 1000) {
+            interval = 1000;
+        }
 
-SliderJS.prototype.next = function () {
-    if (this._slides.length < 1) {
-        return;
-    }
+        this.next();
+        this._autoplayKey = setInterval(this.next.bind(this), interval);
+    };
 
-    if (this._playing) 
-    {
-         return;
-    }
+    pause() {
+        if (!this._autoplayKey) {
+            return;
+        }
 
-    this._playing = true;
+        clearInterval(this._autoplayKey);
+        this._autoplayKey = null;
 
-    let prevIndex = this.getPrevIndex(this._currentSlideIndex);
-    this._slides[prevIndex].style.display = 'none';
+        let autoplayLink = this._topContainer.querySelector('.slidesjs-autoplay');
+        if (autoplayLink) {
+            autoplayLink.style.display = 'inline'
+        }
 
-    this._slides[this._currentSlideIndex].style.left = `-${this._width}px`;
+        let pauseLink = this._topContainer.querySelector('.slidesjs-pause');
+        if (pauseLink) {
+            pauseLink.style.display = 'none'
+        }
+    };
 
-    let nextIndex = this.getNextIndex(this._currentSlideIndex);
+    prev() {
+        if (this._slides.length < 1) {
+            return;
+        }
 
-    let nextNextIndex = this.getNextIndex(nextIndex);
-    if (this._slides.length > 3) {
-        this._slides[nextNextIndex].style.display = 'block';
-    }
-    this._slides[nextNextIndex].style.left = `${this._width}px`;
+        if (this._playing) 
+        {
+            return;
+        }
 
-    this._slides[nextIndex].style.display = 'block';
-    this._slides[nextIndex].style.left = '0px';
+        this._playing = true;
+    
+        let nextIndex = this.getNextIndex(this._currentSlideIndex);
+        this._slides[nextIndex].style.display = 'none';
 
-    this._currentSlideIndex = nextIndex;
+        this._slides[this._currentSlideIndex].style.left = `${this._width}px`;
+        this._slides[this._currentSlideIndex].style.zIndex = '10';
+
+        let prevIndex = this.getPrevIndex(this._currentSlideIndex);
+
+        let prevPrevIndex = this.getPrevIndex(prevIndex);
+
+        let nextNextIndex = this.getNextIndex(nextIndex);
+        if (this._slides.length > 3) {
+            this._slides[prevPrevIndex].style.display = 'block';
+        }
+        this._slides[prevPrevIndex].style.left = `-${this._width}px`;
+
+        this._slides[prevIndex].style.display = 'block';
+        this._slides[prevIndex].style.left = '0px';
+
+        this._currentSlideIndex = prevIndex;
+    };
+
+    next() {
+        if (this._slides.length < 1) {
+            return;
+        }
+
+        if (this._playing) 
+        {
+            return;
+        }
+
+        this._playing = true;
+
+        let prevIndex = this.getPrevIndex(this._currentSlideIndex);
+        this._slides[prevIndex].style.display = 'none';
+
+        this._slides[this._currentSlideIndex].style.left = `-${this._width}px`;
+
+        let nextIndex = this.getNextIndex(this._currentSlideIndex);
+
+        let nextNextIndex = this.getNextIndex(nextIndex);
+        if (this._slides.length > 3) {
+            this._slides[nextNextIndex].style.display = 'block';
+        }
+        this._slides[nextNextIndex].style.left = `${this._width}px`;
+
+        this._slides[nextIndex].style.display = 'block';
+        this._slides[nextIndex].style.left = '0px';
+
+        this._currentSlideIndex = nextIndex;
+    };
 };
